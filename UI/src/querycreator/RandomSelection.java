fd2df3a11c8b2b5bc8992142253f0a94390b1459
isuruanucse <isuruanucse@gmail.com>
2011-05-12 04:07:30 +0000

    query creator algorithms shifted to a new package.

---
+++ UI/src/querycreator/RandomSelection.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package querycreator;
+
+/**
+ *
+ * @author Brave Heart
+ */
+public class RandomSelection {
+
+    float selectedSentenceRatio = 0.4f;
+
+    public void setSelectedSentenceRatio(float ratio) {
+        if(ratio > 0.0 && ratio < 1.0)
+        {
+            this.selectedSentenceRatio = ratio;
+        }
+    }
+
+    public float getSelectedSentenceRatio() {
+        return this.selectedSentenceRatio;
+    }
+}
