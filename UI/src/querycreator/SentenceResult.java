4b30f37201c70e0f223b986fb49a6b795031338f
kasuncjayasinghe <kasuncjayasinghe@gmail.com>
2011-05-22 06:42:27 +0000

    Improved the filtering algorithm.

---
+++ UI/src/querycreator/SentenceResult.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package querycreator;
+
+/**
+ *
+ * @author kasun
+ */
+public class SentenceResult {
+   String sentence;
+   Double score;
+}
