607f161e074651ae74b00c02385f53c82b864073
kasuncjayasinghe <kasuncjayasinghe@gmail.com>
2011-06-21 21:28:04 +0000

    DocumentManagerForm data binded.

---
+++ UI/src/gui/form/SelectedFile.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui.form;
+
+/**
+ *
+ * @author Kasun
+ */
+public class SelectedFile {
+    String fileName;
+    String location;
+}
