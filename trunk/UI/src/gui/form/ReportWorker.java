150c0ac94fe8ba0f8869b43e2a3381c91068ed85
uchathuranga@gmail.com <uchathuranga@gmail.com@3d2eb647-2f8a-b1c0-f951-46bd6487b315>
2011-08-31 05:17:05 +0000

    project folder deletion done

--- trunk/UI/src/gui/form/ReportWorker.java
+++ trunk/UI/src/gui/form/ReportWorker.java
@@ -4,6 +4,7 @@
  */
 package gui.form;
 
+import java.io.File;
 import javax.swing.SwingWorker;
 import reportingModule.ReportingModule;
 import reportingModule.MsgBox;
@@ -12,15 +13,17 @@ import reportingModule.MsgBox;
  * @author Compaq
  */
 public class ReportWorker extends SwingWorker<ReportingModule, String> {
-
+    File projectFolder ;
     String selectedDocumentPath;
     ReportData repdata;
     ReportingModule rp = new ReportingModule();
+    boolean deletefolder;
 
-    public ReportWorker(String aselectedDocumentPath, ReportData arepdata) {
+    public ReportWorker(String aselectedDocumentPath, ReportData arepdata, File aprojectFolder,boolean deletefolder) {
         this.repdata = arepdata;
         this.selectedDocumentPath = aselectedDocumentPath;
-
+        this.projectFolder = aprojectFolder;
+        this.deletefolder = deletefolder;
     }
 
     @Override
@@ -34,7 +37,7 @@ public class ReportWorker extends SwingWorker<ReportingModule, String> {
         rp.setTemp(repdata.getFolder());
         rp.setUrl(repdata.getUrlList());
         rp.setMap(repdata.getFileUrlMap());
-        rp.setData();
+        rp.setData(projectFolder,deletefolder);
         System.err.print("end");
         //msg.setVisible(false);
 
