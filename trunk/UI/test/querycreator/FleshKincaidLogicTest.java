c00469a8b27158f5d771d1759d2eddc3527c314a
kasuncjayasinghe@gmail.com <kasuncjayasinghe@gmail.com@3d2eb647-2f8a-b1c0-f951-46bd6487b315>
2011-09-03 15:47:48 +0000

    Modified some methods.

--- trunk/UI/test/querycreator/FleshKincaidLogicTest.java
+++ trunk/UI/test/querycreator/FleshKincaidLogicTest.java
@@ -35,32 +35,17 @@ public class FleshKincaidLogicTest {
     @After
     public void tearDown() {
     }
-    /**
-     * Test of getCountingFragments method, of class FleshKincaidLogic.
-     */
-    @Test
-    public void testGetCountingFragments() {
-        System.out.println("getCountingFragments");
-        FleshKincaidLogic instance = new FleshKincaidLogic();
-        boolean expResult = false;
-        boolean result = instance.getCountingFragments();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of getWordsInFile method, of class FleshKincaidLogic.
      */
     @Test
     public void testGetWordsInFile() {
-        System.out.println("getWordsInFile");
         FleshKincaidLogic instance = new FleshKincaidLogic();
-        int expResult = 0;
+        instance.processString("This is a test.");
+        int expResult = 4;
         int result = instance.getWordsInFile();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -68,13 +53,11 @@ public class FleshKincaidLogicTest {
      */
     @Test
     public void testGetSentencesInFile() {
-        System.out.println("getSentencesInFile");
         FleshKincaidLogic instance = new FleshKincaidLogic();
-        int expResult = 0;
+        instance.processString("This is a test,!? or not.");
+        int expResult = 2;
         int result = instance.getSentencesInFile();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -82,27 +65,11 @@ public class FleshKincaidLogicTest {
      */
     @Test
     public void testGetSyllablesInFile() {
-        System.out.println("getSyllablesInFile");
         FleshKincaidLogic instance = new FleshKincaidLogic();
-        int expResult = 0;
+        instance.processString("He is confident of the results.");
+        int expResult = 9;
         int result = instance.getSyllablesInFile();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getSyllablesPerWordInFile method, of class FleshKincaidLogic.
-     */
-    @Test
-    public void testGetSyllablesPerWordInFile() {
-        System.out.println("getSyllablesPerWordInFile");
-        FleshKincaidLogic instance = new FleshKincaidLogic();
-        double expResult = 0.0;
-        double result = instance.getSyllablesPerWordInFile();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -115,62 +82,5 @@ public class FleshKincaidLogicTest {
         double expResult = 19.808181818181826;
         double result = instance.getFleschKincaidGradeLevel();
         assertEquals(expResult, result, 0.0);
-    }
-
-    /**
-     * Test of getFleschReadingEaseScore method, of class FleshKincaidLogic.
-     */
-    @Test
-    public void testGetFleschReadingEaseScore() {
-        System.out.println("getFleschReadingEaseScore");
-        FleshKincaidLogic instance = new FleshKincaidLogic();
-        double expResult = 0.0;
-        double result = instance.getFleschReadingEaseScore();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAverageWordsPerSentence method, of class FleshKincaidLogic.
-     */
-    @Test
-    public void testGetAverageWordsPerSentence() {
-        System.out.println("getAverageWordsPerSentence");
-        FleshKincaidLogic instance = new FleshKincaidLogic();
-        double expResult = 0.0;
-        double result = instance.getAverageWordsPerSentence();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getWordList method, of class FleshKincaidLogic.
-     */
-    @Test
-    public void testGetWordList() {
-        System.out.println("getWordList");
-        FleshKincaidLogic instance = new FleshKincaidLogic();
-        WordList expResult = null;
-        WordList result = instance.getWordList();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isSentenceValid method, of class FleshKincaidLogic.
-     */
-    @Test
-    public void testIsSentenceValid() {
-        System.out.println("isSentenceValid");
-        String sentence = "";
-        FleshKincaidLogic instance = new FleshKincaidLogic();
-        boolean expResult = false;
-        boolean result = instance.isSentenceValid(sentence);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 }
